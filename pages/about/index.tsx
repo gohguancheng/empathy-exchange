65a72396b462104710181b0d592db3a343ea15a6
guanch92 <guancheng92@gmail.com>
2024-04-17 00:02:13 +0800

    minor clean up on about and start page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -38,24 +38,26 @@ export default function AboutPage() {
           <Accordion
             titleComponent={renderTitle("what", "is this")}
             bodyComponent={renderBody([
+              "Empathy Exchange is a tool that aids users in providing emotional support to their peers while engaging in meaningful sharing and reflection on difficult topics.",
               "Though technology cannot replace the comfort of feeling emotionally supported and listened to by those around us, we believe it can be used to aid us in having healthy discussions and to seek support from those around us.",
-              "Empathy Exchange is a simple tool to aid users in providing emotional support to their peers while engaging in meaningful sharing and reflection on difficult topics.",
             ])}
+            shouldOpen={true}
           />
           <Accordion
             titleComponent={renderTitle("Who", "should use this app")}
             bodyComponent={renderBody([
-              "Anyone who wishes to improve their listening and conversation skills",
-              "Ideal for small teams or friend groups who are doing a regular catch up",
+              "Anyone who wishes to improve their listening and conversation skills.",
+              "Ideal for small teams or friend groups who are doing a regular catch up.",
               "Group sizes are limited to 5 pax to ensure all participants get sufficient time for sharing and conversing.",
             ])}
           />
           <Accordion
             titleComponent={renderTitle("How", "do we start")}
             bodyComponent={renderBody([
-              "Gather in groups of up to 5. Each group should select a Guide who will think of a unique code to create a Space with. Other participants can join the huddle as Peers with this code.",
-              "Participants in the Space will enter a Name to be identified by and a prompt of the Topic that they will be sharing about.",
-              "Participants will also select a listener Role to learn and practise different ways of listening in a converation.",
+              "Gather in groups of up to 5, either in-person (recommended) or on a virtual call.",
+              "Each group should select a Host who will create a Space with a unique passcode. Other participants can join the Space as Peers with this passcode.",
+              "Participants in the Space will enter a Display Name to be identified by and a prompt of the Topic that they will be sharing about.",
+              "Participants will also select a listener Role to learn and practise different ways of engaging in a conversation.",
             ])}
           />
         </div>
