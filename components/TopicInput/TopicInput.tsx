321ae1a68fc29a5a202521bca900ce569ca1eb6f
guanch92 <guancheng92@gmail.com>
2024-04-20 16:49:09 +0800

    revamp topic selection page

--- components/TopicInput/TopicInput.tsx
+++ components/TopicInput/TopicInput.tsx
@@ -1,82 +1,98 @@
 import TextInput from "@/components/TextInput/TextInput";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { IUserData } from "@/utils/types";
 import styles from "@/styles/TopicInput.module.css";
+import { SocketStateContext } from "@/provider/SocketProvider/SocketProvider";
+import { GridButtonsContainer } from "../GridButtonsContainer/GridButtonsContainer";
 
-export const TopicInput = ({ currentUser, onSubmit }: TopicInputProps) => {
+export const TopicInput = () => {
+  const { me, setTopic } = useContext(SocketStateContext);
+
   const [input, setInput] = useState("");
   const [status, setStatus] = useState<{ submitted?: boolean; error?: string }>(
     {}
   );
 
-  const label = status.submitted
-    ? `You will be sharing about "${input}"`
-    : "What would you like to share about?";
+  const label = status.submitted ? `You will be sharing about "${input}"` : "";
 
   useEffect(() => {
-    if (!!currentUser?.topic) {
-      setInput(currentUser.topic);
-      setStatus({ submitted: true });
+    if (!!me?.topic) {
+      setInput(me.topic);
     }
+    setStatus({ submitted: !!me?.topic });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [me]);
 
   const renderOptions = () => (
-    <div className={styles.optionsContainer}>
+    <GridButtonsContainer>
       {[
         "Family Issues",
         "Romantic Troubles",
         "Academic Challenges",
         "Career Dilemma",
+        "Financial Stresses",
+        "Identity and Self Esteem",
       ].map((e, i) => (
-        <button
-          key={i}
-          disabled={!!currentUser?.topic}
-          onClick={() => setInput(e)}
-        >
+        <button key={i} disabled={!!me?.topic} onClick={() => setInput(e)}>
           {e}
         </button>
       ))}
-    </div>
+    </GridButtonsContainer>
   );
 
   return (
-    <div className={styles.container}>
-      <h3>{label}</h3>
+    <div
+      key={`${status.submitted}`}
+      className={`${styles.container} slide-fade`}
+    >
+      {!status.submitted ? (
+        <>
+          <h3>What would you like to share with the Space about?</h3>
 
-      {!currentUser?.topic && renderOptions()}
+          {renderOptions()}
 
-      <form
-        className={styles.form}
-        onSubmit={(e) => {
-          e.preventDefault();
-          setStatus({ submitted: true });
-          onSubmit(input);
-        }}
-      >
-        <TextInput
-          label="Topic"
-          value={input}
-          onChange={(value) => {
-            setStatus({});
-            setInput(value);
-          }}
-          onValidate={(value) => {
-            if (value && value.length < 5) {
-              setStatus({ error: "Let's share a little more" });
-            }
-          }}
-          delay={200}
-          disabled={status.submitted}
-          errorMessage={status.error}
-        />
+          <form
+            className={styles.form}
+            onSubmit={(e) => {
+              e.preventDefault();
+              setStatus({ submitted: true });
+              setTopic(input);
+            }}
+          >
+            <TextInput
+              label="Sharing Topic"
+              value={input}
+              onChange={(value) => {
+                setStatus({});
+                setInput(value);
+              }}
+              onValidate={(value) => {
+                if (value && value.length < 5) {
+                  setStatus({ error: "Let's share a little more" });
+                }
+              }}
+              delay={200}
+              disabled={status.submitted}
+              errorMessage={status.error}
+            />
 
-        <input
-          type="submit"
-          value="Submit"
-          disabled={!input || !!status.error || !!status.submitted}
-        />
-      </form>
+            <input
+              type="submit"
+              value="Submit"
+              disabled={!input || !!status.error || !!status.submitted}
+            />
+          </form>
+        </>
+      ) : (
+        <>
+          <div className={styles.submittedContainer}>
+            <h3>{`You have decided to share about ${me?.topic}`}</h3>
+            <button onClick={() => setTopic("")} className={styles.reset}>
+              Reset Sharing Topic
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
