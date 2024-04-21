f1725da378c92a29034a9ec10ba4c2f7e18e831e
guanch92 <gohguancheng01@gmail.com>
2024-04-21 23:37:38 +0800

    add new fade carousel component, update styles and clean up unused

---
+++ components/FadeCarousel/FadeCarousel.tsx
@@ -0,0 +1,66 @@
+import { memo, useCallback, useEffect, useRef, useState } from "react";
+import styles from "@/styles/utilities/FadeCarousel.module.css";
+import clsx from "clsx";
+
+export const FadeCarousel = memo(({ list }: Props) => {
+  const [index, setIndex] = useState(0);
+  const time = useRef<NodeJS.Timeout>();
+
+  const updateIndex = useCallback(() => {
+    clearTimeout(time.current);
+    const timeout = setTimeout(() => {
+      setIndex((prev) => (prev === list.length - 1 ? 0 : prev + 1));
+    }, 8000);
+    time.current = timeout;
+    return timeout;
+  }, []);
+
+  useEffect(() => {
+    updateIndex();
+
+    return () => {
+      clearTimeout(time.current);
+      time.current = undefined;
+    };
+  }, [index]);
+
+  return (
+    <div
+      className={styles.container}
+      onTouchStart={() => clearTimeout(time.current)}
+      onTouchEnd={() => updateIndex()}
+      onMouseEnter={() => clearTimeout(time.current)}
+      onMouseLeave={() => updateIndex()}
+    >
+      <div
+        className={clsx("fade",{ [styles.invisible]: index === 0 })}
+        onClick={() => {
+          if (index === 0) return;
+          clearTimeout(time.current);
+          setIndex((p) => p - 1);
+        }}
+      >
+        ⬅️
+      </div>
+      {list.map((str, i) => (
+        <p key={i} className={clsx("fade", { [styles.hide]: index !== i })}>
+          {str}
+        </p>
+      ))}
+      <div
+        className={clsx("fade",{ [styles.invisible]: index === list.length - 1 })}
+        onClick={() => {
+          if (index === list.length - 1) return;
+          clearTimeout(time.current);
+          setIndex((p) => p + 1);
+        }}
+      >
+        ➡️
+      </div>
+    </div>
+  );
+});
+
+type Props = {
+  list: string[];
+};
