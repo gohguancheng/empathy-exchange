9012e640151d6d7f84d2c9eced7b4defe4442216
guanch92 <gohguancheng01@gmail.com>
2024-04-21 23:38:36 +0800

    update roles and sharing dashboard

--- lib/roles.ts
+++ lib/roles.ts
@@ -4,27 +4,41 @@ export const roles = {
   [ERole.EMPATHISER]: {
     label: "Empathiser",
     description: [
-      "You try to understand and share the feelings of those speaking.",
-      "You offer compassion, support, and a listening ear to them.",
-      "In this role, you provide comfort and reassurance to those facing challenges, helping them feel understood and less alone in their struggles.",
+      "Try to understand and share the feelings of those speaking",
+      "Offer compassion, support, and a listening ear to others",
+      "Provide words of comfort and reassurance",
+      "Let others feel understood and less alone in their struggles",
     ],
     emoji: "🤗",
   },
   [ERole.REFLECTOR]: {
     label: "Reflector",
     description: [
-      "You mirror back the thoughts and emotions expressed by others, fostering deeper understanding and clarity in conversations.",
-      " By paraphrasing and summarizing what others say, you help them gain insight into their own feelings and experiences, promoting self-awareness and personal growth.",
+      "Mirror back the thoughts and emotions expressed by speakers back to them",
+      "Paraphrase what others say to help them gain insight into their own feelings and experiences",
+      "Promote self-awareness by allowing others to hear and reflect on what they said",
     ],
     emoji: "💬",
   },
   [ERole.SUMMARISER]: {
     label: "Summariser",
     description: [
-      "You distill complex information into clear and concise summaries.",
-      "Through synthesizing key points from discussions and providing comprehensive overviews, you practice organizing thoughts and ideas effectively.",
-      "By actively engaging in this process, you develop the skill of communicating main takeaways succinctly, facilitating collaboration and understanding among participants.",
+      "Distill complex information into clear and concise summaries",
+      "Simplify important points from discussions and provide comprehensive overviews",
+      "Practice organizing thoughts and ideas",
+      "Develop the skill of communicating main takeaways succinctly",
     ],
     emoji: "📋",
   },
+};
+
+export const speakerRole = {
+  label: "Speaker",
+  description: [
+    "Remember that effective communication involves understanding the capacity of your listeners",
+    "Provide sufficient warning before sharing details that may be uncomfortable",
+    "Avoid trauma dumping or sharing in an overly aggressive manner",
+    "Be considerate of the duration of your sharing",
+  ],
+  emoji: "🎤",
 };
