6d0986bc28f4bb20bc1cbd0e097d5db1b949ec05
guanch92 <gohguancheng01@gmail.com>
2024-06-29 14:44:41 +0800

    update string sanitization logic and gitignore

--- utils/string.ts
+++ utils/string.ts
@@ -1,5 +1,5 @@
 export function hasXSSChars(str: string): boolean {
-  const xssChars = /<|>|"|'|&|\/|\+|\\|\0/;
+  const xssChars = /#|\||;|:|<|>|"|'|&|\/|\+|\\|\0/;
   return xssChars.test(str);
 }
 
